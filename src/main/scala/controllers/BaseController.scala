0b8021158271e37ec0d6810751a56a3f79acfbc4
Mark Rudolph <mark@k8ty.app>
2022-09-27 23:15:30 -0400

    More scaladoc

--- src/main/scala/controllers/BaseController.scala
+++ src/main/scala/controllers/BaseController.scala
@@ -10,7 +10,10 @@ import zio.Task
 trait BaseController {
 
   /** A collection of routes that will be aggregated, and served in
-    * [[_root_.Main]]
+    * [[_root_.Main]]. Since we should be building our Controller
+    * implementations with the service module pattern, our R should be fixed at
+    * Any here, and we won't have to worry about widening any Types when
+    * aggregating.
     */
   val routes: List[ServerEndpoint[Any, Task]] = List.empty
 
