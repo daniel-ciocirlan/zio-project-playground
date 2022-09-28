fe7663f43208919b29d6a4bc59a940a6555b5161
Mark Rudolph <mark@k8ty.app>
2022-09-27 23:05:24 -0400

    Start adding scaladoc before it's too late

--- src/main/scala/domain/errors/ApplicationError.scala
+++ src/main/scala/domain/errors/ApplicationError.scala
@@ -1,13 +1,29 @@
 package domain.errors
 
+/** A root exception for all of our errors to extend
+  * @param message
+  * @param cause
+  */
 sealed abstract class ApplicationError(message: String, cause: Throwable)
     extends Exception(message, cause)
 
+/** A root exception for all of our database related errors to extend
+  * @param message
+  * @param cause
+  */
 abstract class DatabaseException(message: String, cause: Throwable)
     extends ApplicationError(message, cause)
 
+/** A root exception for all of our http-server related errors to extend
+  * @param message
+  * @param cause
+  */
 abstract class ApplicationServerException(message: String, cause: Throwable)
     extends ApplicationError(message, cause)
 
+/** An error for when you're about to need to implement a new error class :-)
+  * @param message
+  * @param cause
+  */
 final case class UnknownException(message: String, cause: Throwable)
     extends ApplicationError(message, cause)
