3ffadfab4748e77114a0d8bfbe2fd212a17fd318
Mark Rudolph <mark@k8ty.app>
2023-01-23 10:00:11 -0500

    add form submission

--- app/src/main/scala/pages/CompaniesPage.scala
+++ app/src/main/scala/pages/CompaniesPage.scala
@@ -6,6 +6,11 @@ import com.raquo.laminar.api.L._
 object CompaniesPage {
 
   def apply(): HtmlElement = Page(
-    h1("Company stuff here")
+    h1("Company stuff here"),
+    a(
+      className := "btn btn-outline-info m-2",
+      href      := "/companies/create",
+      "Add New Company"
+    )
   )
 }
