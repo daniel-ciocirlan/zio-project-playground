390e2a062d4b3d0586983f43ac07b930cef3348e
Mark Rudolph <mark@k8ty.app>
2023-02-26 22:39:08 -0500

    submit review

--- app/src/main/scala/pages/AddReviewPage.scala
+++ app/src/main/scala/pages/AddReviewPage.scala
@@ -2,11 +2,17 @@ package pages
 
 import com.raquo.laminar.api.L._
 import components.{FormSelect, NumberInput, TextInput}
+import domain.api.request.CreateReviewRequest
 import helpers.FormHelper.stateWriter
+import helpers.ZJS
+import helpers.ZJS._
+import io.frontroute.BrowserNavigation
 import layouts.Page
+import zio.ZIO
 object AddReviewPage {
 
   case class FormState(
+      companyId: Long = -1,
       mgt: Int = 0,
       culture: Int = 0,
       salary: Int = 0,
@@ -16,11 +22,14 @@ object AddReviewPage {
       showErrors: Boolean = false,
       upstreamError: Option[String] = None
   ) {
+
+    def hasErrors: Boolean = upstreamError.isDefined
+
     def clearErrors: FormState =
       this.copy(showErrors = false, upstreamError = None)
 
     def errorMessage: Option[String] =
-      Option.empty
+      upstreamError
 
   }
 
@@ -64,7 +73,40 @@ object AddReviewPage {
 
   }
 
-//  val submitter: Observer[FormState] = ???
+  val submitter: Observer[FormState] =
+    Observer[FormState] { state =>
+      if (state.hasErrors) {
+        stateVar.update(_.copy(showErrors = true))
+      } else {
+        stateVar.update(_.copy(showErrors = false))
+        (
+          for {
+            _ <- ZJS.client(
+                   _.createReview(
+                     CreateReviewRequest(
+                       companyId = state.companyId,
+                       management = state.mgt,
+                       culture = state.culture,
+                       salary = state.salary,
+                       benefits = state.benefits,
+                       wouldRecommend = state.wouldRecommend,
+                       review = state.review
+                     )
+                   )
+                 )
+          } yield {
+            stateVar.set(FormState())
+            BrowserNavigation.replaceState(s"/companies/${state.companyId}")
+          }
+        ).tapError { case e =>
+          ZIO.succeed(
+            stateVar.update(
+              _.copy(showErrors = true, upstreamError = Option(e.getMessage))
+            )
+          )
+        }.runJs
+      }
+    }
 
   val ratingRange: Seq[Int] =
     (0 to 5)
@@ -83,22 +125,40 @@ object AddReviewPage {
       )
     ),
     form(
-      FormSelect("Management", ratingRange, stateWriter = mtgWriter)(_.toInt),
-      FormSelect("Culture", ratingRange, stateWriter = cultureWriter)(_.toInt),
-      FormSelect("Benefits", ratingRange, stateWriter = benefitsWriter)(
+      onSubmit.preventDefault
+        .mapTo(stateVar.now()) --> submitter,
+      FormSelect("Management", ratingRange, 0, mtgWriter)(
+        _.toInt
+      ),
+      FormSelect("Culture", ratingRange, 0, cultureWriter)(
+        _.toInt
+      ),
+      FormSelect("Benefits", ratingRange, 0, benefitsWriter)(
         _.toInt
       ),
       FormSelect(
         "Overall Recommendation",
         ratingRange,
-        stateWriter = recommendWriter
+        0,
+        recommendWriter
       )(_.toInt),
       NumberInput("Salary (approximate)", salaryWriter),
-      TextInput("Review", reviewWriter)
+      TextInput("Review", reviewWriter, className := "mb-2"),
+      p(
+        button(
+          `type`    := "submit",
+          className := "btn btn-primary",
+          "Submit"
+        )
+      )
     )
   )
 
   def apply(companyId: Long): HtmlElement = Page(
-    formContent
+    div(
+      formContent,
+      onMountCallback(_ => stateVar.set(FormState(companyId = companyId))),
+      onUnmountCallback(_ => stateVar.set(FormState()))
+    )
   )
 }
