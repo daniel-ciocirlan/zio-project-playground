d1799d492249e0559852847bea7f2ef99c35ed9a
Mark Rudolph <mark@k8ty.app>
2023-02-26 23:38:26 -0500

    build out reviews

--- app/src/main/scala/pages/CompanyPage.scala
+++ app/src/main/scala/pages/CompanyPage.scala
@@ -1,14 +1,102 @@
 package pages
 
-import layouts.Page
 import com.raquo.laminar.api.L._
+import domain.api.response.Review
 import helpers.ZJS
-import helpers.ZJS.ExtendedZIO
+import helpers.ZJS.{AppEnv, ExtendedZIO}
+import layouts.Page
+import zio.ZIO
+
+import scala.math.BigDecimal.RoundingMode
 object CompanyPage {
 
   val content: Var[HtmlElement] = Var[HtmlElement](div())
 
-  def buildContent(id: String) = {
+  private def ratingHelper(ratings: Seq[Int]): String = {
+    BigDecimal
+      .decimal(ratings.sum.toDouble / ratings.size.toDouble)
+      .setScale(1, RoundingMode.UP)
+      .toString() + "/5"
+  }
+
+  private def ratingHelper(rating: Int): String = ratingHelper(Seq(rating))
+
+  private def overviewBuilder(reviews: Seq[Review]): Div = {
+    if (reviews.isEmpty) {
+      div(
+        p(
+          "No reviews yet!"
+        )
+      )
+    } else {
+      div(
+        p(
+          className := "card-text",
+          s"Overall recommendation: ${ratingHelper(reviews.map(_.wouldRecommend))}"
+        ),
+        p(
+          className := "card-text",
+          s"Management: ${ratingHelper(reviews.map(_.management))}"
+        ),
+        p(
+          className := "card-text",
+          s"Culture: ${ratingHelper(reviews.map(_.culture))}"
+        ),
+        p(
+          className := "card-text",
+          s"Benefits: ${ratingHelper(reviews.map(_.benefits))}"
+        )
+      )
+    }
+
+  }
+  private def reviewBuilder(reviews: Seq[Review]): Div = {
+    if (reviews.isEmpty) {
+      div()
+    } else {
+      div(
+        h5(
+          "All Reviews:"
+        ),
+        reviews.map { r =>
+          div(
+            className := "card mb-2",
+            div(
+              className := "card-body",
+              p(
+                className := "card-text",
+                s"Would recommend: ${ratingHelper(r.wouldRecommend)}"
+              ),
+              p(
+                className := "card-text",
+                s"Management: ${ratingHelper(r.management)}"
+              ),
+              p(
+                className := "card-text",
+                s"Culture: ${ratingHelper(r.culture)}"
+              ),
+              p(
+                className := "card-text",
+                s"Benefits: ${ratingHelper(r.benefits)}"
+              ),
+              p(
+                className := "card-text",
+                s"Salary: $$${r.salary / 1000}k"
+              ),
+              h5("Review"),
+              p(
+                className := "card-text",
+                r.review
+              )
+            )
+          )
+        }
+      )
+
+    }
+  }
+
+  def buildContent(id: String): ZIO[AppEnv, Throwable, Unit] = {
     for {
       c       <- ZJS
                    .client(_.getCompanyById(id))
@@ -16,35 +104,25 @@ object CompanyPage {
       reviews <- ZJS.client(_.getReviewsByCompanyId(c.id))
     } yield {
       div(
-        className := "card",
         div(
-          className := "card-body",
-          h5(className := "card-title", c.name),
-          p(
-            className  := "card-text",
-            "review summary will go here..."
-          ),
-          ul(
-            className  := "list-group list-group-flush",
-            // TODO if no reviews, suggest adding one
-            reviews.map { r =>
-              li(
-                className := "list-group-item",
-                r.review // TODO map this to a better component
-              )
-            }
-          ),
-          a(
-            className  := "card-link",
-            href       := c.url,
-            "Website"
-          ),
-          a(
-            className  := "card-link",
-            href       := s"/reviews/add/${c.id}",
-            "Add Review"
+          className := "card mb-2",
+          div(
+            className := "card-body",
+            h5(className := "card-title", c.name),
+            overviewBuilder(reviews),
+            a(
+              className  := "card-link",
+              href       := c.url,
+              "Company Website"
+            ),
+            a(
+              className  := "card-link",
+              href       := s"/reviews/add/${c.id}",
+              "Add Review"
+            )
           )
-        )
+        ),
+        reviewBuilder(reviews)
       )
     }
   }.map(s => content.set(s))
